90e0f47e6add4bff36995362d773e49a1e9d42d8
s2171135 <s2171135@s.konan-u.ac.jp>
2022-11-11 16:59:33 +0900

    FirstSetting

---
+++ app/src/main/java/com/example/puzzleworld/Title_Scene.java
@@ -0,0 +1,14 @@
+package com.example.puzzleworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Title_Scene extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title__scene);
+    }
+}

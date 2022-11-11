30a0f4faecc38c50c62e58c738019822af93f3af
s2171033 <s2171033`s.konan-u.ac.jp>
2022-11-11 17:48:56 +0900

    ボタンを配置して、タイトル画面からステージセレクト画面に遷移できるようにした。

--- app/src/main/java/com/example/puzzleworld/StageSelectScene.java
+++ app/src/main/java/com/example/puzzleworld/StageSelectScene.java
@@ -10,5 +10,7 @@ public class StageSelectScene extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stage_select_scene);
+
+
     }
 }

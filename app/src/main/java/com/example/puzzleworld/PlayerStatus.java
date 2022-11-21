e286eddaae1eb2cdf7b07a031c3d9462ea00d2fb
thousand0625 <88626463+NoranekoSan1000@users.noreply.github.com>
2022-11-21 16:50:49 +0900

    リザルトに送る用の変数追加

--- app/src/main/java/com/example/puzzleworld/PlayerStatus.java
+++ app/src/main/java/com/example/puzzleworld/PlayerStatus.java
@@ -3,4 +3,8 @@ package com.example.puzzleworld;
 public class PlayerStatus {
     public static int GachaStone = 10;//ガチャ石
     public static int CanPlayStage = 1; //現在進めるステージ数
+
+    //GameScene --> resultScene用
+    public static boolean GameClear = false;
+    public static int LastPhase = 0;
 }

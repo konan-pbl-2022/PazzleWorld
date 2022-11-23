647bfdaa1640d2d87cf6f4e02fa86c66a31a4658
Tsugu <s2171132@s.konan-u.ac.jp>
2022-11-23 10:06:08 +0900

    ガチャ石の表示 排出率ボタンの完成 ガチャを引くときに石が必要な設定に

--- app/src/main/java/com/example/puzzleworld/GachaScene.java
+++ app/src/main/java/com/example/puzzleworld/GachaScene.java
@@ -1,11 +1,14 @@
 package com.example.puzzleworld;
 
+import static com.example.puzzleworld.PlayerStatus.GachaStone;
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
@@ -18,24 +21,26 @@ public class GachaScene extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gacha_scene);
 
+        /*ガチャ石の表示*/
+        TextView stone = (TextView)findViewById(R.id.stoneView);
+        stone.setText("ガチャ石："+String.valueOf(GachaStone));
 
 
         /*排出率表示のボタン*/
-//        ToggleButton emi = (ToggleButton) findViewById(R.id.emiButton);
-//        emi.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-//            @Override
-//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-//                if(isChecked){
-//
-//                }
-//            }
-//        } {
-//            @Override
-//            public void onClick(View view) {
-//                TextView emiRateView = (TextView)findViewById(R.id.emiRateView);
-//                emiRateView.setText("\"SSR\" ----> 2.5% × 4種 \\n\"SR\" ----> 5% × 4種\\n\"R\" ----> 7.5% × 4種\\n \"N\" ----> 12.5% × 4種\\n\"");
-//            }
-//        });
+        Switch emi = (Switch) findViewById(R.id.emiButton);
+        TextView emiRateView = (TextView)findViewById(R.id.emiRateView);
+        emi.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    emiRateView.setVisibility(View.VISIBLE);
+                    TextView emiRateView = (TextView)findViewById(R.id.emiRateView);
+                    emiRateView.setText("'SSR' ----> 2.5% × 4種 \n 'SR' ----> 5% × 4種\n'R' ----> 7.5% × 4種 \n'N' ----> 12.5% × 4種");
+                }else {
+                    emiRateView.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
 
 //        /*ガチャるボタンの処理*/
 //        Button gachaButton = (Button)findViewById(R.id.button1);
@@ -48,10 +53,10 @@ public class GachaScene extends AppCompatActivity {
 //                ok.setOnClickListener(new View.OnClickListener() {
 //                    @Override
 //                    public void onClick(View view) {
-////                        Intent intent = new Intent(GachaScene.this./*GachaResult*/.class);
-////                            startActivity(intent);
-////                    }
-////                });
+//                        Intent intent = new Intent(GachaScene.this./*GachaResult*/.class);
+//                            startActivity(intent);
+//                    }
+//                });
 //                        Button back = (Button)findViewById(R.id.backButton);
 //                        back.setOnClickListener(new View.OnClickListener() {
 //                            @Override
@@ -87,56 +92,66 @@ public class GachaScene extends AppCompatActivity {
         protoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /*結果表示*/
-                TextView ResultView = (TextView)findViewById(R.id.ResultView);
-                //double rarity = Math.random(); /*浮動小数点ver*/
-                //double type = Math.random(); /*浮動小数点ver*/
-                Random rand1 = new Random();
-                int rarity = rand1.nextInt(99) + 1; /*レアリティ*/
-                Random rand2 = new Random();
-                int type = rand2.nextInt(99) + 1; /*種別*/
+                if(GachaStone > 0){
+                    PlayerStatus.GachaStone -= 1;
+                    /*ガチャ石の表示の修正*/
+                    stone.setText("ガチャ石："+String.valueOf(GachaStone));
 
-                /*ガチャの排出率調整と処理*/
-                if(rarity < 10) { //10%の確率で"SSR"
-                    if(type < 25) {
-                        ResultView.setText("SSR1");
-                    }else if(type < 50){
-                        ResultView.setText("SSR2");
-                    }else if(type < 75){
-                        ResultView.setText("SSR3");
-                    }else{
-                        ResultView.setText("SSR4");
+
+                    /*結果表示*/
+                    TextView ResultView = (TextView)findViewById(R.id.ResultView);
+                    Random rand1 = new Random();
+                    int rarity = rand1.nextInt(99) + 1; /*レアリティ*/
+                    Random rand2 = new Random();
+                    int type = rand2.nextInt(99) + 1; /*種別*/
+
+                    /*ガチャの排出率調整と処理*/
+                    if(rarity < 10) { //10%の確率で"SSR"
+                         if(type < 25) {
+                            ResultView.setText("SSR1");
+                        }else if(type < 50){
+                            ResultView.setText("SSR2");
+                        }else if(type < 75){
+                            ResultView.setText("SSR3");
+                        }else{
+                            ResultView.setText("SSR4");
+                        }
+
+                    }else if(rarity < 30){ //20%の確率で"SR"
+                        if(type < 25) {
+                            ResultView.setText("SR1");
+                        }else if(type < 50){
+                            ResultView.setText("SR2");
+                        }else if(type < 75){
+                            ResultView.setText("SR3");
+                        }else{
+                            ResultView.setText("SR4");
+                        }
+
+                    }else if(rarity < 60){ //30%の確率で"R"
+                        if(type < 25) {
+                            ResultView.setText("R1");
+                        }else if(type < 50){
+                            ResultView.setText("R2");
+                        }else if(type < 75){
+                            ResultView.setText("R3");
+                        }else{
+                            ResultView.setText("R4");
+                        }
+                    }else{ //それ以外(40%)は"N"
+                        if(type < 25) {
+                            ResultView.setText("N1");
+                        }else if(type < 50){
+                            ResultView.setText("N2");
+                        }else if(type < 75){
+                            ResultView.setText("N3");
+                        }else{
+                            ResultView.setText("N4");
+                        }
                     }
-                }else if(rarity < 30){ //20%の確率で"SR"
-                    if(type < 25) {
-                        ResultView.setText("SR1");
-                    }else if(type < 50){
-                        ResultView.setText("SR2");
-                    }else if(type < 75){
-                        ResultView.setText("SR3");
-                    }else{
-                        ResultView.setText("SR4");
-                    }
-                }else if(rarity < 60){ //30%の確率で"R"
-                    if(type < 25) {
-                        ResultView.setText("R1");
-                    }else if(type < 50){
-                        ResultView.setText("R2");
-                    }else if(type < 75){
-                        ResultView.setText("R3");
-                    }else{
-                        ResultView.setText("R4");
-                    }
-                }else{ //それ以外(40%)は"N"
-                    if(type < 25) {
-                        ResultView.setText("N1");
-                    }else if(type < 50){
-                        ResultView.setText("N2");
-                    }else if(type < 75){
-                        ResultView.setText("N3");
-                    }else{
-                        ResultView.setText("N4");
-                    }
+                }else{
+                    TextView ResultView = (TextView)findViewById(R.id.ResultView);
+                    ResultView.setText("ガチャ石が足りません");
                 }
             }
         });
--- app/src/main/java/com/example/puzzleworld/PlayerStatus.java
+++ app/src/main/java/com/example/puzzleworld/PlayerStatus.java
@@ -1,6 +1,6 @@
 package com.example.puzzleworld;
 
 public class PlayerStatus {
-    public int GachaStone = 10;//ガチャ石
+    public static int GachaStone = 10;//ガチャ石
     public int CanPlayStage = 1; //現在進めるステージ数
 }

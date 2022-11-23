8183f1b3916017319cb17a755d245e38e1682f50
Tsugu <s2171132@s.konan-u.ac.jp>
2022-11-23 10:47:12 +0900

    ガチャ石の表示 排出率ボタンの完成 ガチャを引くときに石が必要な設定にした 「ガチャる」を押すと確認の画面が出てくるようにした

--- app/src/main/java/com/example/puzzleworld/GachaScene.java
+++ app/src/main/java/com/example/puzzleworld/GachaScene.java
@@ -3,6 +3,7 @@ package com.example.puzzleworld;
 import static com.example.puzzleworld.PlayerStatus.GachaStone;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,9 +11,7 @@ import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.ToggleButton;
 
-import java.text.BreakIterator;
 import java.util.Random;
 
 public class GachaScene extends AppCompatActivity {
@@ -42,30 +41,187 @@ public class GachaScene extends AppCompatActivity {
             }
         });
 
-//        /*ガチャるボタンの処理*/
-//        Button gachaButton = (Button)findViewById(R.id.button1);
-//        gachaButton.setOnClickListener(new View.OnClickListener() {
+        /*確認の初期値*/
+        TextView check = (TextView)findViewById(R.id.checkText);
+        Button ok = (Button)findViewById(R.id.okButton);
+        Button cancel = (Button)findViewById(R.id.cancelButton);
+        check.setVisibility(View.INVISIBLE);
+        ok.setVisibility(View.INVISIBLE);
+        cancel.setVisibility(View.INVISIBLE);
+        TextView ResultView = (TextView)findViewById(R.id.ResultView);
+
+        /*ガチャるボタンの処理*/
+        Button gachaButton = (Button)findViewById(R.id.gachaButton);
+        gachaButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                TextView check = (TextView)findViewById(R.id.checkText);
+                Button ok = (Button)findViewById(R.id.okButton);
+                Button cancel = (Button)findViewById(R.id.cancelButton);
+
+                /*確認の表示とガチャボタンの非表示*/
+                check.setVisibility(View.VISIBLE);
+                ok.setVisibility(View.VISIBLE);
+                cancel.setVisibility(View.VISIBLE);
+                check.setText("石を１つ使ってガチャを引きますか？");
+                gachaButton.setVisibility(View.INVISIBLE);
+                ResultView.setVisibility(View.INVISIBLE);
+
+                /*okを押すと*/
+                ok.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        check.setVisibility(View.INVISIBLE);
+                        ok.setVisibility(View.INVISIBLE);
+                        cancel.setVisibility(View.INVISIBLE);
+                        gachaButton.setVisibility(View.VISIBLE);
+                        ResultView.setVisibility(View.VISIBLE);
+
+                        if(GachaStone > 0){
+                            PlayerStatus.GachaStone -= 1;
+                            /*ガチャ石の表示の修正*/
+                            stone.setText("ガチャ石："+String.valueOf(GachaStone));
+
+                            /*結果表示*/
+                            Random rand1 = new Random();
+                            int rarity = rand1.nextInt(99) + 1; /*レアリティ*/
+                            Random rand2 = new Random();
+                            int type = rand2.nextInt(99) + 1; /*種別*/
+
+                            /*ガチャの排出率調整と処理*/
+                            if(rarity < 10) { //10%の確率で"SSR"
+                                if(type < 25) {
+                                    ResultView.setText("SSR1");
+                                }else if(type < 50){
+                                    ResultView.setText("SSR2");
+                                }else if(type < 75){
+                                    ResultView.setText("SSR3");
+                                }else{
+                                    ResultView.setText("SSR4");
+                                }
+
+                            }else if(rarity < 30){ //20%の確率で"SR"
+                                if(type < 25) {
+                                    ResultView.setText("SR1");
+                                }else if(type < 50){
+                                    ResultView.setText("SR2");
+                                }else if(type < 75){
+                                    ResultView.setText("SR3");
+                                }else{
+                                    ResultView.setText("SR4");
+                                }
+
+                            }else if(rarity < 60){ //30%の確率で"R"
+                                if(type < 25) {
+                                    ResultView.setText("R1");
+                                }else if(type < 50){
+                                    ResultView.setText("R2");
+                                }else if(type < 75){
+                                    ResultView.setText("R3");
+                                }else{
+                                    ResultView.setText("R4");
+                                }
+                            }else{ //それ以外(40%)は"N"
+                                if(type < 25) {
+                                    ResultView.setText("N1");
+                                }else if(type < 50){
+                                    ResultView.setText("N2");
+                                }else if(type < 75){
+                                    ResultView.setText("N3");
+                                }else{
+                                    ResultView.setText("N4");
+                                }
+                            }
+                        }else{
+                            TextView ResultView = (TextView)findViewById(R.id.ResultView);
+                            ResultView.setText("ガチャ石が足りません");
+                        }
+                    }
+                });
+
+                /*cancelを押すと*/
+                cancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        check.setVisibility(View.INVISIBLE);
+                        ok.setVisibility(View.INVISIBLE);
+                        cancel.setVisibility(View.INVISIBLE);
+                        gachaButton.setVisibility(View.VISIBLE);
+                        ResultView.setVisibility(View.VISIBLE);
+                    }
+                });
+            }
+        });
+
+//        /*ガチャボタンの処理*/
+//        Button pull = (Button)findViewById(R.id.gachaButton);
+//        pull.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
-//                TextView check = (TextView)findViewById(R.id.checkText);
-//                check.setText("石を１つ使ってガチャを引きますか？");
-//                Button ok = (Button)findViewById(R.id.okButton);
-//                ok.setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View view) {
-//                        Intent intent = new Intent(GachaScene.this./*GachaResult*/.class);
-//                            startActivity(intent);
+//                if(GachaStone > 0){
+//                    PlayerStatus.GachaStone -= 1;
+//                    /*ガチャ石の表示の修正*/
+//                    stone.setText("ガチャ石："+String.valueOf(GachaStone));
+//
+//                    /*結果表示*/
+//                    TextView ResultView = (TextView)findViewById(R.id.ResultView);
+//                    Random rand1 = new Random();
+//                    int rarity = rand1.nextInt(99) + 1; /*レアリティ*/
+//                    Random rand2 = new Random();
+//                    int type = rand2.nextInt(99) + 1; /*種別*/
+//
+//                    /*ガチャの排出率調整と処理*/
+//                    if(rarity < 10) { //10%の確率で"SSR"
+//                        if(type < 25) {
+//                            ResultView.setText("SSR1");
+//                        }else if(type < 50){
+//                            ResultView.setText("SSR2");
+//                        }else if(type < 75){
+//                            ResultView.setText("SSR3");
+//                        }else{
+//                            ResultView.setText("SSR4");
+//                        }
+//
+//                    }else if(rarity < 30){ //20%の確率で"SR"
+//                        if(type < 25) {
+//                            ResultView.setText("SR1");
+//                        }else if(type < 50){
+//                            ResultView.setText("SR2");
+//                        }else if(type < 75){
+//                            ResultView.setText("SR3");
+//                        }else{
+//                            ResultView.setText("SR4");
+//                        }
+//
+//                    }else if(rarity < 60){ //30%の確率で"R"
+//                        if(type < 25) {
+//                            ResultView.setText("R1");
+//                        }else if(type < 50){
+//                            ResultView.setText("R2");
+//                        }else if(type < 75){
+//                            ResultView.setText("R3");
+//                        }else{
+//                            ResultView.setText("R4");
+//                        }
+//                    }else{ //それ以外(40%)は"N"
+//                        if(type < 25) {
+//                            ResultView.setText("N1");
+//                        }else if(type < 50){
+//                            ResultView.setText("N2");
+//                        }else if(type < 75){
+//                            ResultView.setText("N3");
+//                        }else{
+//                            ResultView.setText("N4");
+//                        }
 //                    }
-//                });
-//                        Button back = (Button)findViewById(R.id.backButton);
-//                        back.setOnClickListener(new View.OnClickListener() {
-//                            @Override
-//                            public void onClick(View view) {
-//                                /*戻したい*/
-//                            }
-//                        });
-//                    }
-//                });
+//                }else{
+//                    TextView ResultView = (TextView)findViewById(R.id.ResultView);
+//                    ResultView.setText("ガチャ石が足りません");
+//                }
+//            }
+//        });
 
         /*ステージ画面へ*/
         Button toStage = (Button)findViewById(R.id.ToStage);
@@ -84,75 +240,6 @@ public class GachaScene extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(GachaScene.this,MemberSelectScene.class);
                 startActivity(intent);
-            }
-        });
-
-        /*ガチャボタンの処理*/
-        Button protoButton = (Button)findViewById(R.id.protoButton);
-        protoButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(GachaStone > 0){
-                    PlayerStatus.GachaStone -= 1;
-                    /*ガチャ石の表示の修正*/
-                    stone.setText("ガチャ石："+String.valueOf(GachaStone));
-
-
-                    /*結果表示*/
-                    TextView ResultView = (TextView)findViewById(R.id.ResultView);
-                    Random rand1 = new Random();
-                    int rarity = rand1.nextInt(99) + 1; /*レアリティ*/
-                    Random rand2 = new Random();
-                    int type = rand2.nextInt(99) + 1; /*種別*/
-
-                    /*ガチャの排出率調整と処理*/
-                    if(rarity < 10) { //10%の確率で"SSR"
-                         if(type < 25) {
-                            ResultView.setText("SSR1");
-                        }else if(type < 50){
-                            ResultView.setText("SSR2");
-                        }else if(type < 75){
-                            ResultView.setText("SSR3");
-                        }else{
-                            ResultView.setText("SSR4");
-                        }
-
-                    }else if(rarity < 30){ //20%の確率で"SR"
-                        if(type < 25) {
-                            ResultView.setText("SR1");
-                        }else if(type < 50){
-                            ResultView.setText("SR2");
-                        }else if(type < 75){
-                            ResultView.setText("SR3");
-                        }else{
-                            ResultView.setText("SR4");
-                        }
-
-                    }else if(rarity < 60){ //30%の確率で"R"
-                        if(type < 25) {
-                            ResultView.setText("R1");
-                        }else if(type < 50){
-                            ResultView.setText("R2");
-                        }else if(type < 75){
-                            ResultView.setText("R3");
-                        }else{
-                            ResultView.setText("R4");
-                        }
-                    }else{ //それ以外(40%)は"N"
-                        if(type < 25) {
-                            ResultView.setText("N1");
-                        }else if(type < 50){
-                            ResultView.setText("N2");
-                        }else if(type < 75){
-                            ResultView.setText("N3");
-                        }else{
-                            ResultView.setText("N4");
-                        }
-                    }
-                }else{
-                    TextView ResultView = (TextView)findViewById(R.id.ResultView);
-                    ResultView.setText("ガチャ石が足りません");
-                }
             }
         });
     }
